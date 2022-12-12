202867371fa13e066883aaaeeb614d4941e41b2c
Eli Weston <elijweston@gmail.com>
2022-12-12 12:25:12 -0700

    WIP

---
+++ resources/js/Welcome.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>welcome page from vue.js</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Hello World</h1>
+    <h2>{{title}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  setup: () => ({
+    title: 'Hopefully this works'
+  })
+}
+</script>
